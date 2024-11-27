96a557fd395f995b464afd13c4daa30e9c4ff41f
migueldariodiaz <migueldariodiaz2016@gmail.com>
2024-11-27 12:46:11 -0300

    Se realizo el modulo numero 2 del curso de Python en mouredev.pro donde se vieron cosas relacionadas a los operadores: aritmeticos, comparativos y logicos.

---
+++ 2_operators/operators.py
@@ -0,0 +1,49 @@
+### Operadores ###
+
+# Operadores aritmeticos
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+# Operadores aritmeticos con strings
+print("Hola" + "Python")
+print("Hola" + " " + "Python")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+# Operadores comparativos con numeros
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+# Operadores comparativos con strings
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+print("Hola" == "HOLA")
+print("Hola" >= "Bola")
+print("aaaa" >= "aaaa")
+print("aaaa" >= "abaa")
+print("aaaa" >= "aaaa")
+
+# Opradores logicos
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or "Hola" > "Python" and 4 == 4)
+print(3 > 4 and not("Hola" > "Python"))
